e98207eb78ef0593d4a66a7f3668bfe0c61c8548
iilhmstd <dc.muhammadilham@gmail.com>
2025-06-11 19:51:29 +0700

    penambahan LOGIN system

--- perpustakaan_mini.java
+++ perpustakaan_mini.java
@@ -5,6 +5,9 @@ public class perpustakaan_mini {
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
 
+        String[] adminUsername = {"admin1", "superuser", "admin2", "admin3"};
+        String[] adminPassword = {"password123", "sipaling admin", "admin ke2", "admin magang"};
+
         System.out.println(" =======================================");
         System.out.println(" ========== PERPUSTAKAAN MINI ==========");
         System.out.println(" =======================================");
@@ -13,7 +16,22 @@ public class perpustakaan_mini {
         System.out.print("username : "); String USERNAME = br.readLine();
         System.out.print("password : "); String PASSWORD = br.readLine();
 
-        if ()
+        boolean isAdmin = false;
+
+        for (int i = 0; i < adminUsername.length; i++) {
+            if (adminUsername[i].equals(USERNAME) && adminPassword[i].equals(PASSWORD)) {
+                isAdmin = true;
+                break;
+            }
+        }
+
+        if (isAdmin) {
+            System.out.println("Anda berhasil LOGIN sebagai ADMIN!");
+        } else {
+            System.out.println("Anda Berhasil LOGIN sebagai user");
+        }
+
+        br.close();
 
 
     }
